4c21ead8d4725ca663c69df7836cef55a727a010
SAFVAN EKKADAN <131762440+safvanekkadan@users.noreply.github.com>
2023-04-26 10:02:09 +0530

    Update email.js

--- email.js
+++ email.js
@@ -1,13 +1,19 @@
+
 function sendMail() {
+  let nameValue = document.getElementById("name").value;
+  let emailValue = document.getElementById("email").value;
+
+  if(nameValue.length>0 && emailValue.length>0){
+
     var params = {
       name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       message: document.getElementById("message").value,
     };
+    
     const serviceID ="service_ymvdakc";
     const  templateID ="template_95ovjlr";
     
-  
       emailjs.send(serviceID, templateID, params).then(res=>{
           document.getElementById("name").value = "";
           document.getElementById("email").value = "";
@@ -17,5 +23,11 @@ function sendMail() {
   
       })
       .catch(err=>console.log(err));
+  }else if(nameValue.length>0 && emailValue.length<=0){
+    alert('Fill the Email')
+  }else{
+    alert('Fill the Name')
+  }
+   
   
   }
